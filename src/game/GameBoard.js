f7a95373a5f3d47d6f8b6845b3a36a70c00a4af6
BentlyM <metayerbently@gmail.com>
2024-07-23 14:04:16 -0400

    included notes and TDD test for gameboard

---
+++ src/game/GameBoard.js
@@ -0,0 +1,48 @@
+class GameBoard {
+  constructor() {
+    this.size = 10;
+    this.ships = [];
+    this.board = [];
+  }
+
+  placeShip(Ship, x, y, orientation) {
+    if (x + Ship.length > this.size || y + Ship.length > this.size)
+      return new Error('Ship exceeds board boundaries');
+
+    for (let i = 0; i < Ship.length; i++) {
+      if (orientation === "horizontal") {
+        if (this.board[x + i][y] != "")
+          return new Error('ship overlaps with existing ship (x)');
+      } else {
+        if (this.board[x][y + i] != "")
+          return new Error('ship overlaps with existing ship (y)');
+      }
+    }
+
+    for(let i = 0; i < Ship.length; i++){
+        if(orientation == "horizontal"){
+            this.board[x + i][y] = Ship;
+        }else{
+            this.board[x][y + i] = Ship;
+        }
+    }
+
+    this.ships.push(Ship);
+
+    return 'success';
+  }
+
+  printBoard() {
+    for (let i = 0; i < this.size; i++) {
+      const rows = [];
+      for (let j = 0; j < this.size; j++) {
+        rows.push('');
+      }
+      this.board.push(rows);
+    }
+
+    return this.board;
+  }
+}
+
+export default GameBoard;
---
+++ src/game/game.test.js
@@ -0,0 +1,51 @@
+import GameBoard from './GameBoard';
+import Ship from './Ship';
+
+describe('My ship', () => {
+  it('is not sunk initially', () => {
+    const firstShip = new Ship(4);
+    expect(firstShip.isSunk()).toBe(false);
+  });
+
+  it('is sunk after 4 hits', () => {
+    const firstShip = new Ship(4);
+    firstShip.hit();
+    firstShip.hit();
+    firstShip.hit();
+    firstShip.hit();
+    expect(firstShip.isSunk()).toBe(true);
+  });
+});
+
+describe('My GameBoard', () => {
+  it('is 10 rows long', () => {
+    const arena = new GameBoard();
+    expect(arena.printBoard().length).toBe(10);
+  });
+
+  it('is able to dock ship in ships', () => {
+    const arena = new GameBoard();
+    const expected = [{ Sunk: false, hits: 0, length: null }];
+    arena.placeShip(new Ship());
+
+    expect(arena.ships).toEqual(expect.arrayContaining(expected));
+  });
+
+  describe('My Board', () => {
+    const arena = new GameBoard();
+
+    it('is able to check Ship out of Bounds (x)', () => {
+      expect(arena.placeShip(new Ship(3), 10, 0, 'horizontal')).toStrictEqual(
+        new Error('Ship exceeds board boundaries')
+      );
+    });
+    it('is able to check Ship out of Bounds (y)', () => {
+      expect(arena.placeShip(new Ship(3), 0, 10, 'vertical')).toStrictEqual(
+        new Error('Ship exceeds board boundaries')
+      );
+    });
+    it('is able to sail within Bounds', () => {
+      expect(arena.placeShip(new Ship(3), 5, 3, 'vertical')).toEqual('success');
+    });
+  });
+});
