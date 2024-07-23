eacc56c320d6cf53e7412b40da34a546bc35f44f
BentlyM <metayerbently@gmail.com>
2024-07-23 13:43:54 -0400

    created ship object information

---
+++ src/game/Ship.js
@@ -0,0 +1,21 @@
+class Ship {
+    constructor(length = null){
+        this.length = length;
+        this.hits = 0;
+        this.Sunk = false;
+    }
+
+    hit(){
+        this.hits++;
+        if(this.hits >= this.length){
+            this.Sunk = true;
+        }
+    }
+
+    isSunk(){
+        return this.Sunk;
+    }
+}
+
+
+export default Ship;
