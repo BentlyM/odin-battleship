e9388a1cf456be3f0e3f44e6c06986842db678bb
BentlyM <metayerbently@gmail.com>
2024-07-24 13:28:31 -0400

    "Added Ship import and implementation to GameBoard class, including receiveAttack and placeShip methods, and updated game.test.js with new tests for GameBoard"

--- src/game/GameBoard.js
+++ src/game/GameBoard.js
@@ -1,21 +1,59 @@
+import Ship from './Ship';
+
 class GameBoard {
   constructor() {
     this.size = 10;
     this.ships = [];
-    this.board = [];
+    this.board = Array.from({ length: this.size }, () =>
+      Array(this.size).fill('')
+    );
   }
 
-  printBoard() {
-    for (let i = 0; i < this.size; i++) {
-      const rows = [];
-      for (let j = 0; j < this.size; j++) {
-        rows.push('');
+  receiveAttack(x , y){
+
+  }
+
+  placeShip(shipLength, x, y, orientation) {
+    if (x + shipLength > this.size || y + shipLength > this.size)
+      return new Error('Ship exceeds board boundaries');
+
+    for (let i = 0; i < shipLength; i++) {
+      if (orientation === 'horizontal') {
+        if (this.board[x + i][y] != '')
+          return new Error('ship overlaps with existing ship (x)');
+      } else {
+        if (this.board[x][y + i] != '')
+          return new Error('ship overlaps with existing ship (y)');
       }
-      this.board.push(rows);
     }
 
-    return this.board;
+    for (let i = 0; i < shipLength; i++) {
+      if (orientation == 'horizontal') {
+        this.board[x + i][y] = 'Ship';
+      } else {
+        this.board[x][y + i] = 'Ship';
+      }
+    }
+
+    for (let i = 0; i < shipLength; i++) {
+      const ship = new Ship(shipLength);
+      const shipPortion = { isHit: false, x: null, y: null };
+
+      if (orientation == 'horizontal') {
+        shipPortion['x'] = x + i;
+        shipPortion['y'] = y;
+      } else {
+        shipPortion['x'] = x;
+        shipPortion['y'] = y + 1;
+      }
+
+      this.ships.push(ship);
+      ship.portions.push(shipPortion);
+    }
+
+    return 'success';
   }
 }
+
 
 export default GameBoard;
--- src/game/game.test.js
+++ src/game/game.test.js
@@ -1,3 +1,4 @@
+import GameBoard from './GameBoard';
 import Ship from './Ship';
 
 describe('My ship', () => {
@@ -16,3 +17,61 @@ describe('My ship', () => {
   });
 });
 
+describe('GameBoard', () => {
+  describe('initBoard', () => {
+    it('initializes a board of correct size', () => {
+      const gameBoard = new GameBoard();
+      const board = gameBoard.board;
+
+      expect(board.length).toBe(10);
+      expect(board[0].length).toBe(10);
+
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+          expect(board[i][j]).toBe('');
+        }
+      }
+    });
+  });
+
+  describe('placeShip', ()=>{
+    test('is able sail within board', () => { 
+      const gameBoard = new GameBoard();
+      const ship = new Ship(3);
+      const expected = [
+        {
+          hits: 0,
+          length: 3,
+          portions: [{
+          isHit: false,
+          x: 2,
+          y: 3
+        }],
+          Sunk: false
+        }, {
+          hits: 0,
+          length: 3,
+          portions: [{
+          isHit: false,
+          x: 3,
+          y: 3
+        }],
+          Sunk: false
+        }, {
+          hits: 0,
+          length: 3,
+          portions: [{
+          isHit: false,
+          x: 4,
+          y: 3
+        }],
+          Sunk: false
+        }
+      ]
+
+      expect(gameBoard.placeShip(ship.length, 2, 3, "horizontal")).toEqual('success');
+      expect(gameBoard.ships).toEqual(expect.arrayContaining(expected));
+     })
+  })
+
+});
