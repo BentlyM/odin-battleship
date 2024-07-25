2f45160c39f4adb6e2a23492da5653eb367063b0
BentlyM <metayerbently@gmail.com>
2024-07-25 13:57:49 -0400

    Added data-x and data-y attributes to gridItem elements in showBoard function. Added showBoard function to display boards in the dome. included styling in both styles and gameboard, included html

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,10 +1,14 @@
+import { showBoard } from "../utils/dom";
 import { Computer , Player } from "./Player"
 
 export const Play = () => {
     const Bot = new Computer();
-    const User = new Player();
+    Bot.id = 'bot-board';
+    showBoard(Bot);
 
-    
+    const User = new Player();
+    User.id = 'player-board'
+    showBoard(User);
 
     return {
         
---
+++ src/utils/dom.js
@@ -0,0 +1,28 @@
+/**
+ * Renders the game board in the DOM.
+ * 
+ * @param {Object} board - The game board object.
+ */
+
+export const showBoard = (board) => {
+    const boardElement = document.querySelector(`#${board.id}`);
+    const boardData = board.Control.board;
+    let count = 1;
+  
+    boardData.forEach((row, rowIndex) => {
+      const gridRow = document.createElement('tr');
+      boardElement.appendChild(gridRow);
+  
+      const gridNumber = document.createElement('th');
+      gridRow.appendChild(gridNumber);
+      gridNumber.innerText = count++;
+  
+      row.forEach((cell, columnIndex) => {
+        const gridItem = document.createElement('td');
+        gridRow.appendChild(gridItem);
+        
+        gridItem.setAttribute('data-x' , rowIndex)
+        gridItem.setAttribute('data-y' , columnIndex)
+      });
+    });
+  };
